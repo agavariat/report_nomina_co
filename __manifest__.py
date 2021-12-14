e837fc5519934cafc3d1f96a57cf6f593d612c42
Intresco S.A.S <44479766+agavariat@users.noreply.github.com>
2021-12-14 17:54:30 -0500

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Report Nómina Colombia',
+    'version': '0.1',
+    'category': 'Payroll',
+    'summary': 'Payslip Report',
+    'description': """
+Payslip Report
+============================================================
+    """,
+    'depends': [
+        'l10n_co_payroll',
+    ],
+    'data': [
+        'report/report.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
